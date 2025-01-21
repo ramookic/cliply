a65e9bbbbc74e7419e30587f9859622f3a7d6b43
Ramo Okic <okicramo9@gmail.com>
2025-01-21 19:31:59 +0100

    feat(link): add getUserLink function for fetching authenticated user's link based on link id

--- src/lib/data-service.ts
+++ src/lib/data-service.ts
@@ -2,6 +2,7 @@ import isAuthenticated from "@/utils/supabase/is-authenticated";
 import getAllLinks from "./links/get-all-links";
 import { redirect } from "next/navigation";
 import { Tables } from "../../types_db";
+import getLink from "./links/get-link";
 
 /**
  * Fetches all the links associated with the authenticated user.
@@ -13,6 +14,26 @@ export const getUserLinks = async (): Promise<Tables<"links">[] | null> => {
   const user = await isAuthenticated();
 
   const { data, error } = await getAllLinks({ userId: user.id });
+
+  if (error) {
+    redirect(`/dashboard?error=${error.message}`);
+  }
+
+  return data;
+};
+
+/**
+ * Fetches a link associated with the authenticated user based on linkId.
+ * @returns {Promise<Tables<"links">[] | null>} A promise that resolves to the user's link or redirects in case of an error.
+ * @throws {Error} If the user is not authenticated or if there is an error fetching the link.
+ */
+
+export const getUserLink = async (
+  linkId: number
+): Promise<Tables<"links"> | null> => {
+  const user = await isAuthenticated();
+
+  const { data, error } = await getLink({ userId: user.id, linkId });
 
   if (error) {
     redirect(`/dashboard?error=${error.message}`);
