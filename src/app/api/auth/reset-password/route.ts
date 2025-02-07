7667ad8ac040b7be7108210c25b2e642708f81d6
ramookic <okicramo9@gmail.com>
2025-02-07 12:59:07 +0100

    feat(auth): add reset password API endpoint

---
+++ src/app/api/auth/reset-password/route.ts
@@ -0,0 +1,84 @@
+import resetPassword from "@/lib/auth/reset-password";
+import { schema } from "@/schemas/reset-password-schema";
+import { createClient } from "@/utils/supabase/server";
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+/**
+ * Handles pasword reset.
+ *
+ * - Checks if the user is already logged in.
+ * - Parses and validates the request body against the resetpassword schema.
+ * - Sends an email to user using `resetPassword` function.
+ * - Returns appropriate responses based on the outcome.
+ *
+ * @param {NextRequest} req - The incoming request object.
+ * @returns {Promise<NextResponse>} - A JSON response with the result.
+ *
+ * Status codes:
+ * - 200: Success, check your email!.
+ * - 400: Validation failed, password reset failed, or user already logged in.
+ * - 500: Unexpected server error.
+ */
+
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  try {
+    const supabase = await createClient();
+
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+
+    if (session) {
+      return NextResponse.json(
+        {
+          message: "You are already logged in.",
+        },
+        { status: 400 }
+      );
+    }
+
+    const body = await req.json();
+
+    const { email } = schema.parse(body);
+
+    const reqHeaders = await headers();
+
+    const host = reqHeaders.get("host") || "localhost:3000";
+    const protocol = host.includes("localhost") ? "http" : "https";
+
+    const redirectTo = `${protocol}://${host}/api/auth/update-password`;
+
+    const { error } = await resetPassword(email, redirectTo);
+
+    if (error) {
+      return NextResponse.json(
+        { message: "Password reset failed", errors: error.message },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        message: "Success, check your email!",
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        {
+          message: "Validation failed",
+          errors: error.errors[0].message,
+        },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+}
